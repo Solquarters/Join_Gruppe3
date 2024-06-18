fbdbc586a1f5549ca61ba30f891be4aecbb6649e
Maurice Preißler <m.preissler@etik.com>
2024-06-18 12:42:02 +0200

    add contact.js + update board CSS

---
+++ js/05contactsScript.js
@@ -0,0 +1,19 @@
+let contactsJSON =
+[
+    {
+        firstName: "Bob",
+        lastName: "Johnson",
+        phone: "0123 55 66 77",
+        email: "bob@johnson.com"
+    },
+    {
+         firstName: "Fred",
+        lastName: "Theman",
+        phone: "0123 55 66 77",
+        email: "fred@man.com"
+    },
+    {
+       ...
+    },
+
+];
