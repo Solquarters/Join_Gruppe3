a254a22e43a1961e2bd8953df73752dd0a8412f4
Solquarters <rom.kabucov@gmail.com>
2024-06-19 20:14:16 +0200

    01 script js , a lot board html and js

--- js/03boardScript.js
+++ js/03boardScript.js
@@ -1,28 +1,102 @@
-let
 
-                function boardToDoTemplate(categoryCard, categoryColorCode, titleCard, descriptionCard, ID, prioCard){
-                    return /*html*/`
-                    <div id="${ID}" draggable="true" ondragstart="startDragging(${ID})" 
-                    onclick="openTaskOverview(${ID}, '${categoryCard}')" class="board_task_container" >
-                        <div id="innerContainer${ID}" class="board_task_container_inner">
-                            <div class="board_task_container_category" style="background-color: ${categoryColorCode}">${categoryCard}</div>
-                            <div class="board_task_container_title_and_description">
-                                <div class="board_task_container_title">${titleCard}</div>
-                                <div class="board_task_container_description">${descriptionCard}</div>
-                            </div>
-                            <div class="board_task_progress">
-                                <div id="progressParent${ID}">
-                                <div class="board_task_progressbar" id="progressbar${ID}"></div>
-                                </div>
-                                <div class="board_task_progress_text" id="progressbarText${ID}"></div>
-                            </div>
-                            <div class="board_task_assignments">
-                                <div class="board_task_working">
-                                    <div class="icons_container" id="board_icons_username${ID}"></div>
-                                    <div class="board_prio"><img src="../assets/img/${prioCard}.png" /></div>
-                                </div>                            
-                            </div>
-                        </div>
-                    </div> 
-                `
-                }
+function initializeBoard(){
+    renderSingleCard();
+}
+
+function renderSingleCard(){
+    
+    ///Reset all boards
+    document.getElementById('toDoStatusDivId').innerHTML = '';
+    document.getElementById('inProgressStatusDivId').innerHTML = '';
+    document.getElementById('awaitFeedbackStatusDivId').innerHTML = '';
+    document.getElementById('doneStatusDivId').innerHTML = '';
+
+    //Get data from toDoCardsJSON
+    for(let i = 0; i < Object.keys(toDoCardsJSON).length; i++ ){
+
+       console.log(toDoCardsJSON[i]["toDoStatus"]); 
+
+       if(toDoCardsJSON[i]["toDoStatus"] == "To do"){
+        document.getElementById('toDoStatusDivId').innerHTML += returnSingleCardHTML(i);
+       }
+
+       if(toDoCardsJSON[i]["toDoStatus"] == "In progress"){
+        document.getElementById('inProgressStatusDivId').innerHTML += returnSingleCardHTML(i);
+       }
+
+       if(toDoCardsJSON[i]["toDoStatus"] == "Await feedback"){
+        document.getElementById('awaitFeedbackStatusDivId').innerHTML += returnSingleCardHTML(i);
+       }
+
+       if(toDoCardsJSON[i]["toDoStatus"] == "Done"){
+        document.getElementById('doneStatusDivId').innerHTML += returnSingleCardHTML(i);
+       }
+
+    // switch(toDoCardsJSON[i]["toDoStatus"]) {
+    //     case "To do":
+    //         document.getElementById('toDoStatusDivId').innerHTML += returnSingleCardHTML(i);
+    //       break;
+
+    //     case "In progress":
+    //         document.getElementById('inProgressStatusDivId').innerHTML += returnSingleCardHTML(i);
+    //       break;
+
+    //       case "Await feedback":
+    //         document.getElementById('awaitFeedbackStatusDivId').innerHTML += returnSingleCardHTML(i);
+    //       break;
+
+    //       case "Done":
+    //         document.getElementById('doneStatusDivId').innerHTML += returnSingleCardHTML(i);
+    //       break;
+
+    //     default:
+    //       // code block
+    //   }
+
+
+    }
+}
+
+
+function returnSingleCardHTML(i){
+
+    return /*html*/`
+
+    <div class="mainSingleCardDivClass">
+
+        <div class="cardContainerInnert">
+            <div id="cardHeadlineId${i}" class="cardHeadlineClass">
+                <span id="">${toDoCardsJSON[i]["category"]}</span>
+            </div>
+            <div id="containerformularId${i}" class="containerformularDivClass">
+                <span id="">${toDoCardsJSON[i]["title"]}</span>
+            </div>
+
+            <div id="descriptionDivId${i}" class="descriptionDivClass">
+                <span id="descriptionSpanId${i}">${toDoCardsJSON[i]["description"]}</span>
+            </div>
+
+            <br>Progress Bar PLACEHOLDER<br>
+            
+            <div class="contactPrioDiv">
+                <div id="contacts${i}" class="contactCirclesDivClass">
+                    <div class="persons">
+                        <img id="img1" src="./assets/img/Group 9 (1).svg">
+                        <img id="img2" src="./assets/img/Group 9.svg">
+                        <img id="img3" src="./assets/img/Profile badge.svg">
+                    </div>
+                </div>
+
+                <div id="prioDivId${i}">
+                    <div class="prioDiv">
+                        <!-- HIER DYNAMISCH SVG HIER EINFÜGEN, JE NACH CATEGORY -->
+                        <!-- <img src="./assets/img/Priority symbols (1).svg"> -->
+                        Prio:${toDoCardsJSON[i]["prio"]}
+                    </div>
+                </div>     
+            </div>
+        </div>
+    </div>
+    `; 
+
+}
